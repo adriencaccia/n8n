d7b131610c0022c16b8b60d69c4100d8e1f3d317
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2024-05-13 19:24:29 -0400

    chore(codspeed): parallelize runs

--- packages/workflow/test/benchmark.ts
+++ packages/workflow/test/benchmark.ts
@@ -2,17 +2,25 @@ import execa from 'execa';
 import { baseFixtures } from './ExpressionFixtures/base';
 
 async function main() {
+	const promises: Array<Promise<void>> = [];
+
 	for (const [fixtureIndex, fixture] of baseFixtures.entries()) {
 		for (const [testIndex, test] of fixture.tests.entries()) {
 			if ('error' in test || test.type === 'transform') continue;
 
-			const bench = await execa.node('dist/test/benchTest.js', [
-				fixtureIndex.toString(),
-				testIndex.toString(),
-			]);
-			console.log(bench.stdout);
+			promises.push(
+				(async () => {
+					const bench = await execa.node('dist/test/benchTest.js', [
+						fixtureIndex.toString(),
+						testIndex.toString(),
+					]);
+					console.log(bench.stdout);
+				})(),
+			);
 		}
 	}
+
+	await Promise.all(promises);
 }
 
 void main();
