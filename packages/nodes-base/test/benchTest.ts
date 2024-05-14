a59d87ec1ef036bd675e0f7e0dd80a56302691d6
Adrien Cacciaguerra <adrien.caccia@gmail.com>
2024-05-13 23:06:18 -0400

    chore(codspeed): run subset of tests

--- packages/nodes-base/test/benchTest.ts
+++ packages/nodes-base/test/benchTest.ts
@@ -1,22 +1,32 @@
-import glob from 'fast-glob';
-import Bench from 'tinybench';
 import { withCodSpeed } from '@codspeed/tinybench-plugin';
-import { setup, workflowToTests as toTests } from './nodes/Helpers';
+import Bench from 'tinybench';
 import { performExecution, setupExecution } from './nodes/ExecuteWorkflow';
+import { setup, workflowToTests as toTests } from './nodes/Helpers';
 
 const testIndex = parseInt(process.argv[2]);
+const filePaths = [
+	'nodes/Code/test/Code.workflow.json',
+	'nodes/Switch/V1/test/switch.expression.workflow.json',
+	'nodes/Microsoft/Excel/test/v2/node/table/append.workflow.json',
+	'nodes/Microsoft/Teams/test/v2/node/channel/get.workflow.json',
+	'nodes/Microsoft/Excel/test/v2/node/worksheet/readRows.workflow.json',
+	'nodes/ReadPdf/test/ReadPDF.workflow.json',
+	'nodes/Microsoft/Teams/test/v2/node/task/get.workflow.json',
+	'nodes/Discord/test/v2/node/member/roleAdd.workflow.json',
+	'nodes/Discord/test/v2/node/member/getAll.workflow.json',
+	'nodes/Microsoft/Outlook/test/v2/node/folderMessage/getAll.workflow.json',
+];
+const tests = toTests(filePaths);
+const test = tests[testIndex];
+const nodeTypes = setup([test]);
 
 async function main() {
-	const filePaths = await glob('nodes/**/*.workflow.json');
-	const test = toTests(filePaths)[testIndex];
-	const nodeTypes = setup([test]);
-
 	const { waitPromise, additionalData, executionMode, workflowInstance, nodeExecutionOrder } =
 		await setupExecution(test, nodeTypes);
 
 	console.log(`Benchmarking: ${test.description} with pid: ${process.pid}`);
 
-	const bench = withCodSpeed(new Bench({ time: 0, iterations: 1 })); // @TODO temp config
+	const bench = withCodSpeed(new Bench());
 	bench.add(test.description, async () => {
 		await performExecution(
 			waitPromise,
--- packages/nodes-base/test/benchmark.ts
+++ packages/nodes-base/test/benchmark.ts
@@ -1,22 +1,29 @@
 import execa from 'execa';
-import glob from 'fast-glob';
+// import glob from 'fast-glob';
 import { workflowToTests as toTests } from './nodes/Helpers';
 
+const filePaths = [
+	'nodes/Code/test/Code.workflow.json',
+	'nodes/Switch/V1/test/switch.expression.workflow.json',
+	'nodes/Microsoft/Excel/test/v2/node/table/append.workflow.json',
+	'nodes/Microsoft/Teams/test/v2/node/channel/get.workflow.json',
+	'nodes/Microsoft/Excel/test/v2/node/worksheet/readRows.workflow.json',
+	'nodes/ReadPdf/test/ReadPDF.workflow.json',
+	'nodes/Microsoft/Teams/test/v2/node/task/get.workflow.json',
+	'nodes/Discord/test/v2/node/member/roleAdd.workflow.json',
+	'nodes/Discord/test/v2/node/member/getAll.workflow.json',
+	'nodes/Microsoft/Outlook/test/v2/node/folderMessage/getAll.workflow.json',
+];
+console.log(`Found ${filePaths.length} workflows to benchmark`);
+const tests = toTests(filePaths);
+console.log(`Running ${tests.length} tests`);
+
 async function main() {
-	const filePaths = await glob('nodes/**/*.workflow.json');
-
-	console.log(`Found ${filePaths.length} workflows to benchmark`);
-
-	const tests = toTests(filePaths);
-	console.log(`Running ${tests.length} tests`);
+	// const filePaths = await glob('nodes/**/*.workflow.json');
 
 	const promises: Array<Promise<void>> = [];
 
 	for (const [testIndex, _test] of tests.entries()) {
-		// only benchmark every 10th test
-		if (testIndex % 10 !== 0) {
-			continue;
-		}
 		promises.push(
 			(async () => {
 				const bench = await execa.node('dist/test/benchTest.js', [testIndex.toString()]);
@@ -24,6 +31,8 @@ async function main() {
 			})(),
 		);
 	}
+
+	await Promise.all(promises);
 }
 
 void main();
